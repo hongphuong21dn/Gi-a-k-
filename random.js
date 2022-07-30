b301ecc080d1aef0caff7e2fe48c1b36a1f31bdd
hongphuong21dn <109959203+hongphuong21dn@users.noreply.github.com>
2022-07-30 09:01:59 +0700

    Update random.js

--- random.js
+++ random.js
@@ -1,5 +1,5 @@
 document.getElementById('random').onclick = () => {
     const A = ['Quyên','Thảo 25','Thảo','Phượng']
-    const x = Math.round(Math.random()*3);
+    const x = Math.round(Math.random()*4);
     document.getElementById("demo").innerHTML = A[x];
 }
